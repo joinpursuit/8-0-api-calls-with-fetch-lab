305d5b04943cf4b140852337c897fc7c4fe153e6
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-01 13:30:14 -0400

    created trivia question elements & formatted for card class

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ fetch("https://opentdb.com/api.php?amount=10")
     })
     .then((data)=>{
         console.log(data);
+
+        /* create trivia question elements, to format */
+        let triviaCard = document.createElement("article");
+        let triviaCategory = document.createElement("h2");
+        let triviaQuestion = document.createElement("p");
+        let answerButton = document.createElement("button");
+        let correctAnswer = document.createElement("p");
+        triviaCard.setAttribute("class", "card");
+        correctAnswer.setAttribute("class", "hidden");
+        triviaCategory.textContent = "CATEGORY";
+        triviaQuestion.textContent = "QUESTION";
+        answerButton.textContent = "Show Answer";
+        correctAnswer.textContent = "CORRECT ANSWER";
+
+        /* format card - all siblings of `triviaCard` */
+        triviaCard.append(triviaCategory);
+        triviaCard.append(triviaQuestion);
+        triviaCard.append(answerButton);
+        triviaCard.append(correctAnswer);
+        
+        // console.log(triviaCard); // test in console - yes, it matches format
+
     })
     .catch((err)=>{
         console.log(err);
