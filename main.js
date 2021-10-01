d42a486e74d20e390edc0ff3d31fd5a092ed97cb
BennyZ3 <bennyz3@hotmail.com>
2021-10-01 14:44:45 -0400

    added difficulty color, category dropdown for new questions

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-fetch("https://opentdb.com/api.php?amount=10")
-  .then((response) => response.json())
-  .then((result) => {
-    // console.log(makeCard(result.results[0])))
-    result.results.forEach((object) => {
-      document.querySelector("section").append(makeCard(object));
-      console.log(object);
-    });
-  })
-  .catch(console.log);
-
+const baseURL = "https://opentdb.com/api.php?amount=10";
+function apiFetch(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      // console.log(makeCard(result.results[0]));
+      result.results.forEach((object) => {
+        document.querySelector("section").append(makeCard(object));
+        console.log(object);
+      });
+    })
+    .catch(console.log);
+}
+apiFetch(baseURL);
 function makeCard(object) {
   const article = document.createElement("article");
   article.classList.add("card");
+  article.classList.add(object.difficulty);
   const category = document.createElement("h2");
   category.textContent = object.category;
+  const difficulty = document.createElement("h2");
+  difficulty.textContent = object.difficulty;
   const question = document.createElement("p");
   question.innerHTML = object.question;
   const button = document.createElement("button");
@@ -24,9 +30,20 @@ function makeCard(object) {
   const answer = document.createElement("p");
   answer.classList.add("hidden");
   answer.innerHTML = object.correct_answer;
+  article.append(difficulty);
   article.append(category);
   article.append(question);
   article.append(button);
   article.append(answer);
   return article;
 }
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  document.querySelectorAll("article").forEach((card) => card.remove());
+  if (event.target.trivia_category.value !== "any") {
+    apiFetch(baseURL + "&category=" + event.target.trivia_category.value);
+  } else {
+    apiFetch(baseURL);
+  }
+});
