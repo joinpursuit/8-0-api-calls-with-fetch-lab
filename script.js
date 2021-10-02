c7f68854dadeca70eff8ad45b412d461f19b7ff2
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-02 17:07:39 -0400

    fetched new API- Get Memes. Added memebox to store memes on page. Added styles to scroll through

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ fetch("https://opentdb.com/api.php?amount=10")
         return res.json();
     })
     .then((data)=>{
-        console.log(data);
+        // console.log("data:", data);
 
         // get the key of `results` from the fetch data
         let questionList = data.results;
-        console.log(questionList);
+        // console.log("questionList:", questionList);
 
         // create a question, then add it
         for (let askQuestion of questionList){
@@ -42,9 +42,66 @@ fetch("https://opentdb.com/api.php?amount=10")
                 e.preventDefault();
                 correctAnswer.classList.toggle("hidden");
             })
-            
         }
-        
+        /* START fetch another API */
+        return fetch("https://api.imgflip.com/get_memes")
+        .then((response)=>{
+            return response.json();
+        })
+        .then((memeData)=>{
+            console.log("memedata", memeData);
+            let memeImg = document.createElement("img");
+
+            // let memeListed = memeData.data;
+            // console.log("memeListed:", memeData.data);
+
+            let memeDataDataMemes = memeData.data.memes;
+            // console.log("memeDataDataMemes:", memeData.data.memes);
+
+            for(let memeOne of memeDataDataMemes){
+                let memeUrl = memeOne.url;
+                // console.log("memeUrl:", memeUrl);
+
+                /* START - Set <src> to the meme URL */
+                memeUrlFormat = `${memeUrl}`;
+                // console.log("memeUrlFormat", memeUrlFormat);
+
+                // memeImg.setAttribute("src", `${memeUrl}`);
+                // memeImg.src = memeUrlFormat;
+                // console.log("memeImg:", memeImg);
+                /* END - Set <src> to the meme URL */
+
+                /* All of the memes:
+                the memes are in an <img src> tag */
+                let memeImgToPlace = document.createElement("img");
+                console.log("memeUrlFormat", memeImg.src);
+                memeImgToPlace.setAttribute("src", memeUrlFormat);
+                // memeImgToPlace.setAttribute("alt", "meme URL image");
+                // memeImgToPlace.setAttribute("title", memeUrl + ".title");
+                console.log("memeImgToPlace", memeImgToPlace);
+
+                /* Attach `memeImgToPlace` to the page */
+                let memeBox = document.querySelector(".memebox");
+                
+                /* add styles */
+                memeBox.setAttribute("style", "border-top:8px solid red; border-bottom:8px solid red; border-right:40px solid #4f7d5d; border-left:40px solid #4f7d5d; padding:20px; margin:10px; height: 400px; width: 600px; object-fit: fill; overflow: scroll; margin-left:auto; margin-right:auto;");
+
+                /* Add image <img src> to the `memeBox` */
+                memeBox.append(memeImgToPlace);
+
+                /* Format/Style MemeBox header/paragraph text */
+                let memeboxParagraph = document.querySelector("#memebox-paragraph");
+                let memeboxHeader = document.querySelector("#memebox-header");
+
+                memeboxHeader.setAttribute("style", "position:absolute; background-color:red; color:white; padding-left: 50px; padding-top: 10px; padding-bottom: 50px; padding-right: 50px;");
+                memeboxParagraph.setAttribute("style", "position:absolute; background-color:rebeccapurple; color:white; margin-top:50px; padding-left: 50px; padding-right: 50px;");
+            }   
+            })
+            .catch((error)=>{
+                console.log(error);
+        })
+        /* END fetch another API */
+
     })
     .catch((err)=>{
         // let errorMessage = document.createElement("div");
@@ -52,3 +109,22 @@ fetch("https://opentdb.com/api.php?amount=10")
         // mainCentered.append(errorMessage);
         console.log(err);
     })
+
+
+// fetch("https://api.imgflip.com/get_memes")
+//     .then((response)=>{
+//         return response.json();
+//     })
+//     .then((memeData)=>{
+//         // console.log("memedata", memeData);
+//         console.log("theurl", "https://i.imgflip.com/30b1gx.jpg");
+
+//         let memeImg = document.createElement("img");
+//         memeImg.src = memeData[memes.url]
+//         triviaCategory.append(memeImg);
+//         console.log("this", memeImg);
+//         console.log("scream", memeImg.src);
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+// })
