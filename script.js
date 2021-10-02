ff024decd2ea3fddb13a69702a1dc2f3b3fa4b7b
Xiaoming Yang <xiaomingyang@pursuit.com>
2021-10-02 10:07:38 -0400

    create a variable 'trash'

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const removeJunk = (str) => {
     const removeElements = {
-        
+        const trash = {
+            "&quot;": "",
+            "&#039;": `'`,
+            "&eacute;": "e",
+        };
     }
 }
