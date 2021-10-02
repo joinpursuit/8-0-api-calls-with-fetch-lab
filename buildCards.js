9740e8c9101829b0755ce334c84e3f3d5e25f7f5
Rohan-Sterling <rohansterling@pursuit.org>
2021-10-02 10:11:27 -0400

    remove extra variables

--- buildCards.js
+++ buildCards.js
@@ -1,20 +1,12 @@
 
 const makeCards = ({ results }) => {  
-    for(const result of results){
-        console.log(result)
-        document.querySelector("main").innerHTML += `<article class="card">
-        <h2>${result.category}</h2>
-        <p>${result.question}</p>
-        <button>Show Answer</button>
-        <p class="hidden">${result.correct_answer}</p>
-      </article>`
-    }
+    results.forEach(result => {
+        document.querySelector("main").innerHTML += `<article class="card"><h2>${result.category}</h2><p>${result.question}</p><button>Show Answer</button><p class="hidden">${result.correct_answer}</p></article>`
+    })
 
     document.querySelectorAll('article').forEach(article => {
-        article.querySelector("button")
-        .addEventListener('click', () => {
-            const answer = article.querySelector('article > p:last-child')
-            answer.classList.toggle("hidden")
+        article.querySelector("button").addEventListener('click', () => {
+            article.querySelector('article > p:last-child').classList.toggle("hidden")
         })
     })
 }
