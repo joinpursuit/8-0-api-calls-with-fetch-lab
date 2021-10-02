5f9eb47d4c5be32ac721576123caa70eda2da5f9
Xiaoming Yang <xiaomingyang@pursuit.com>
2021-10-02 12:51:23 -0400

    API Calls with Fetch completed by Charlie Moran & Xiaoming Yang

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const removeJunk = (str) => {
     const trash = {
-        "&quot;": "",
-        "&#039;": `'`,    
-        "&eacute;": "e",        
+        "&quot;": '"',
+        "&#039;": `'`,
+        "&eacute;": "é",
     };
 
     const regex = /(&quot;|&#039;|&eacute;)/g;
@@ -11,30 +11,23 @@ const removeJunk = (str) => {
     });
 }
 
-fetch("https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple")
+fetch(
+    "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"
+  )
     .then((response) => response.json())
-    .then(function(trivia_questions) {
-        let questions = trivia_questions.results;
-        for(let quest of questions) {
-            const art = document.createElement("article");
-            art.setAttribute("calss", "card");
-            art.innerHTML =
-            `<h2>${quest.category}</h2>
-             <p>${quest.question}</p>
-             <button>Show Answer</button>
-             <p class = "hidden">${quest.correct_answer}</p>`;
-             art.questions("button").addEventListener("click", (event) => {
-                 
-             })
-        }
-    })
-
-
-
-
-
-
-
-
-
-
+    .then(function (trivia_questions) {
+	let questions = trivia_questions.results;
+      for (let quest of questions) {
+        const art = document.createElement("article");
+        art.setAttribute("class", "card");
+        art.innerHTML = 
+        `<h2>${quest.category}</h2>
+          <p>${quest.question}</p>
+          <button>Show Answer</button>
+          <p class = "hidden">${quest.correct_answer}</p>`;
+        art.querySelector("button").addEventListener("click", (evt) => {
+          evt.target.parentNode.querySelector(".hidden").style.display = "block";
+        });
+        document.querySelector("main").append(art);
+      }
+});
