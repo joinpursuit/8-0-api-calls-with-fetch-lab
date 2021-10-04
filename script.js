53b931419c2f0a9669bf40617cc82e34e19601ef
Jalamang <Jalamangjanka@gmail.com>
2021-10-04 10:59:17 -0400

    Modified

--- script.js
+++ script.js
@@ -17,10 +17,47 @@ document.querySelector('form')
       const article = document.createElement('article'); //creates article element
       article.classList.add('card')
       article.innerHTML = `
-      <h2>${card.results[i].category}</h2>
-      <p>${card.results[i].question}</p>
+      <h2>
+      <select name="trivia_category" class="form-control">
+      <option value="any">Any Category</option>
+      <option value="9">General Knowledge</option>
+      <option value="10">Entertainment: Books</option>
+      <option value="11">Entertainment: Film</option>
+      <option value="12">Entertainment: Music</option>
+      <option value="13">Entertainment: Musicals &amp; Theatres</option>
+      <option value="14">Entertainment: Television</option>
+      <option value="15">Entertainment: Video Games</option>
+      <option value="16">Entertainment: Board Games</option>
+      <option value="17">Science &amp; Nature</option>
+      <option value="18">Science: Computers</option>
+      <option value="19">Science: Mathematics</option>
+      <option value="20">Mythology</option>
+      <option value="21">Sports</option>
+      <option value="22">Geography</option>
+      <option value="23">History</option>
+      <option value="24">Politics</option>
+      <option value="25">Art</option>
+      <option value="26">Celebrities</option>
+      <option value="27">Animals</option>
+      <option value="28">Vehicles</option>
+      <option value="29">Entertainment: Comics</option>
+      <option value="30">Science: Gadgets</option>
+      <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+      <option value="32">Entertainment: Cartoon &amp; Animations</option>       
+      </select>
+  <br>
+  <label for="trivia_difficulty">Select Difficulty: </label>
+  <select name="trivia_difficulty" class="form-control anyDiff">
+      <option value="any">Any Difficulty</option>
+      <option value="easy">Easy</option>
+      <option value="medium">Medium</option>
+      <option value="hard">Hard</option>
+  </select>
+      </h2>
+      <p type = "text" class ="question" style="display:block">${card.results[i].question}</p>
       <button class="answerBtn">Show Answer</button>
-      <p class="hidden">CORRECT ANSWER</p>`
+      <p class="hidden" >${card.results[i].correct_answer}</p>`
+
       main.append(article);
          
       // added a listener on all the displayed show answer buttons  
@@ -41,4 +78,4 @@ document.querySelector('form')
       document.querySelector('form').after(section) // appends section right after form tag
   }
   
-  
+  // <p>${card.results[i].question}</p>
