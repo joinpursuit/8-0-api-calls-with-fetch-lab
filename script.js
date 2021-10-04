37b006d9c78059d42c4a683d06b7f8a8eca80c62
Jenna Qian <jiayin1996@gmail.com>
2021-10-03 23:03:50 -0400

    Unhide button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const main = document.querySelector("main")
+const form = document.querySelector("form")
+const section = document.querySelector("section")
+
 fetch("https://opentdb.com/api.php?amount=10")
 .then((response)=> 
     response.json()
@@ -6,7 +10,6 @@ fetch("https://opentdb.com/api.php?amount=10")
     let triviaResults = data.results
     console.log(triviaResults)
     
-    const form = document.querySelector("form")
     form.addEventListener("submit", (e) => {
         e.preventDefault()
     
@@ -20,17 +23,25 @@ fetch("https://opentdb.com/api.php?amount=10")
             article.append(h2)
         
             let para1 = document.createElement("p")
-            para1.textContent = q.question
+            para1.innerHTML = q.question
+            // console.log(q.question)
             article.append(para1)
             
             let showAnsBtn = document.createElement("button")
             showAnsBtn.textContent = "Show Answer"
             article.append(showAnsBtn)
-        
+
             let para2 = document.createElement("p")
             para2.textContent = q.correct_answer
             para2.setAttribute("class", "hidden")
             article.append(para2)
+
+            main.append(article)
+
+            showAnsBtn.addEventListener("click", (e) => {
+                para2.classList.remove("hidden")
+                para2.innerHTML = q.correct_answer
+            })
         }
     
     })
