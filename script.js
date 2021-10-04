3d54993a4c1d1f6c40ee3dcc8571e4efe867ba6a
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-10-03 20:25:17 -0400

    Test passing / dynamically appended javascript elements to the dom / added documentation to code.

--- script.js
+++ script.js
@@ -12,33 +12,40 @@
                 let questionsList = data.results
 
         for(let list of questionsList){
-                console.log(list);
-
-                let article = document.createElement("article");
+            let main = document.querySelector("body main")
+                
+                /////// Article tag
+                let article = document.createElement("article"); 
                 article.classList = "card";
                 console.log(article)
-
+                
+                /////// h2 tag
                 let h2 = document.createElement("h2");
                 h2.textContent = list.category;
                 article.append(h2);
                 
+                /////// p tag
                 let p = document.createElement("p")
                 p.textContent = list.question;
                 article.append(p)
 
+                /////// button tag
                 let showAnswer = document.createElement("button");
                 showAnswer.textContent = list.correct_answer;
                 article.append(showAnswer);
 
+                /////// p tag with class of "hidden"
                 let hiddenp = document.createElement("p")
                 hiddenp.classList = "hidden";
                 hiddenp.textContent = list.correct_answer;
                 article.append(hiddenp);
 
+                /////// difficulty level
                 let difficulty = list.difficulty;
-                console.log(difficulty)
-                
 
+                ////// dynamically append javascript elements to the main element in Html
+                main.append(article);
+                
             };
         });
     })
