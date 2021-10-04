36b71d0f9f8ce11ff1f25d932dd4ce6e29a5b8ff
Fatema Kabir <fatema.kabir@pursuit.org>
2021-10-03 22:45:38 -0400

    show-answer

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const BASE_URL = "https://opentdb.com/api.php?amount=10";
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  fetch(BASE_URL)
-    .then((response) => response.json())
-    .then((addQuestions))
-    .catch(console.log);
-    event.target.reset();
+        event.preventDefault();
+        fetch(BASE_URL)
+        .then((response) => response.json())
+        .then((addQuestions))
+        .catch(console.log("not working..."));
+        event.target.reset();
 });
 
 const addQuestions = (questions) => {
-    console.log(questions);
     for (let question of questions.results){
         const article = document.createElement("article");
         article.classList.add("card");
-        article.innerHTML = `<h2>${question.category}</h2>
-        <p>${question.question}</p>
-        <button>Show Answer</button>
-        <p class="hidden">${question.correct_answer}</p>`
+        article.innerHTML = 
+        `<h2>${question.category}</h2>
+            <p>${question.question}</p>
+                <button>Show Answer</button>
+                    <p class="hidden">${question.correct_answer}</p>`
         document.querySelector("main").append(article);         
+            const answer = document.querySelector(".card button");
+                article.querySelector("button").addEventListener("click", (event) => {
+                article.querySelector(".hidden").style.display = "block" 
+            });
     };
 };
 
