3265340520636a8f749ed3f4ee2a4d8a9f1187f9
“Lili <lilyhuang114classb@gmail.com>
2021-10-05 00:24:55 -0400

    Updated.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ fetch("https://opentdb.com/api.php?amount=10")
 .then((res) => res.json())
 .then((data) => {
     let list = data.results;
+    console.log(list);
    
     let form = document.querySelector("form");
     form.addEventListener("submit", (e)=> {
@@ -9,7 +10,8 @@ fetch("https://opentdb.com/api.php?amount=10")
 
         for (let info of list) {
             let article = document.createElement("article");
-            article.setAttribute("class","card");
+            // article.setAttribute("class","card");
+            article.className = "card";
             form.append(article);
 
             let h2 = document.createElement("h2");
@@ -25,9 +27,18 @@ fetch("https://opentdb.com/api.php?amount=10")
             article.append(button);
 
             let p2 = document.createElement("p");
-            p2.setAttribute("class","hidden");
+            // p2.setAttribute("class","hidden");
+            p2.className = "hidden";
             p2.textContent = info.correct_answer;
             article.append(p2);
+
+            if(info.difficulty === "hard") article.setAttribute("style","border: 4px solid red");
+            if(info.difficulty === "medium") article.setAttribute("style","border: 3px dotted orange");
+            if(info.difficulty === "easy") article.setAttribute("style","border: 1px solid black");
+
+            button.addEventListener("click", ()=> {
+                p2.classList.remove("hidden");
+            })
         }
     })
 })
