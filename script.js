1b376d26982a3e38c912ba631bf1604517be6a77
theodorefrazier <theo.frazier@icloud.com>
2021-10-05 22:38:20 -0400

    got questions to work

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let mainSection = document.querySelector("main")
+let form = document.querySelector("form")
+
 fetch("https://opentdb.com/api.php?amount=10")
 
     .then((response) =>
@@ -8,47 +11,91 @@ fetch("https://opentdb.com/api.php?amount=10")
     .then((data) => {
 
         let returnedResults = data.results
-        console.log(returnedResults)
-    });
-
-//Sucessfully returning Json object
-
-//selecting main section to build the article element
 
 
-let mainSection = document.querySelector("main")
 
-//Since we know how many elements we are buiilding (ten) we can use a for loop for each instance
-
-for (let i = 0; i < 10; i++) {
-
-/* 
-
-HTML to build - 
-
-<article class="card">
-  <h2>CATEGORY</h2>
-  <p>QUESTION</p>
-  <button>Show Answer</button>
-  <p class="hidden">CORRECT ANSWER</p>
-</article>
+        //Sucessfully returning Json object
 
 
-*/
 
-//building the elements below//
+        //selecting main section to build the article element
 
-let article = document.createElement("article")
-
-let cardHeading = document.createElement("h2")
-
-let questionText = document.createElement("p")
-
-let revealAnswerBtn = document.createElement("button")
-
-let hiddenAnswer = document.createElement("p")
-
-//Asigning attributes to match the HTML provided
+        form.addEventListener("submit", (event) => {
+            event.preventDefault()
 
 
-}
+
+
+            //Since we know how many elements we are buiilding (ten) we can use a for loop for each instance
+
+            for (let question of returnedResults) {
+                console.log(question)
+
+                /* 
+                
+                HTML to build - 
+                
+                <article class="card">
+                  <h2>CATEGORY</h2>
+                  <p>QUESTION</p>
+                  <button>Show Answer</button>
+                  <p class="hidden">CORRECT ANSWER</p>
+                </article>
+                
+                
+                */
+
+                //building the elements below//
+
+                let article = document.createElement("article")
+                article.classList.add("card")
+
+                //H2 heading
+
+                let cardHeading = document.createElement("h2")
+                cardHeading.textContent = question.catagory
+
+                //P tag
+
+                let questionText = document.createElement("p")
+                questionText.innerHTML = question.question
+                //Button to reveal answer
+
+                let revealAnswerBtn = document.createElement("button")
+                revealAnswerBtn.textContent = "Show Answer"
+
+
+                //Hidden text - will connect to an event listener
+
+                let hiddenAnswer = document.createElement("p")
+                hiddenAnswer.classList.add("hidden")
+                hiddenAnswer.textContent = question.correct_answer
+
+
+
+                //Apending each elements to the article in order
+
+                mainSection.append(article)
+
+                article.append(cardHeading)
+
+                article.append(questionText)
+
+                article.append(revealAnswerBtn)
+
+                article.append(hiddenAnswer)
+
+                console.log(returnedResults)
+
+                revealAnswerBtn.addEventListener("click", (event) =>{
+                    //remove the display none of hidden CSS
+                    hiddenAnswer.classList.remove("hidden")
+                    hiddenAnswer.innerHTML = question.correct_answer
+                })
+
+            }
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
