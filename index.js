d518175dffa6e0a1ba73353b7dc6e79c0c28d167
Jonathan Scheiber <jon@scheiber.me>
2022-04-04 16:51:49 -0400

    create initial code to pull and display questions from API

---
+++ index.js
@@ -0,0 +1,46 @@
+const BASE_URL = "https://opentdb.com/api.php?amount=10";
+const main = document.querySelector("main");
+const form = document.querySelector("form");
+const section = document.querySelector("section");
+
+fetch(BASE_URL)
+  .then((response) => response.json())
+  .then((questions) => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      for (const ques of questions.results) {
+        const article = document.createElement("article");
+        article.setAttribute("class", "card");
+        form.append(article);
+
+        const h2 = document.createElement("h2");
+        h2.textContent = ques.category;
+        article.append(h2);
+
+        const p1 = document.createElement("p");
+        p1.innerHTML = ques.question;
+        console.log(ques.question);
+        article.append(p1);
+
+        const p2 = document.createElement("p");
+        p2.textContent = ques.correct_answer;
+        p2.setAttribute("class", "hidden");
+        article.append(p2);
+
+        const showAnswer = document.createElement("button");
+        showAnswer.textContent = "Show Answer";
+        article.append(showAnswer);
+
+        main.append(article);
+
+        showAnswer.addEventListener("click", () => {
+          p2.classList.remove("hidden");
+          p2.innerHTML = ques.correct_answer;
+        });
+      }
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
