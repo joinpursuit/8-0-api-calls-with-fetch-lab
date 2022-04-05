cd53e374ffa609311e8718bf110ad0d2de977e98
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-04-05 01:15:08 -0400

    .

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+let main = document.querySelector("main")
+let article = document.createElement("article")
+main.append(article)
+
+let category = document.createElement("h2")
+article.append(h2)
+category.textContent = object.category
+
+let question = document.createElement("p")
+article.append(p)
+question.textContent = object.question
+
+let button = document.createElem("button")
+article.append(button)
+
+
+let correctAnswer = document.createElement("p")
+article.append(p2)
+
+
+
+
+console.log(data.results)
+
+fetch(https://opentdb.com/api_config.php)
