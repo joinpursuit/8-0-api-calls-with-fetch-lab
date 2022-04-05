d5126f08225214e8aab1088b5c7b62f409fa8ee6
JuHaoChen1997 <juhaochen97@gmail.com>
2022-04-05 15:26:37 -0400

    finish lab, no bonus yet

---
+++ script.js
@@ -0,0 +1,53 @@
+const url = "https://opentdb.com/api.php?amount=10";
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  document.querySelector("main.centered").innerHTML = "";
+  generateTriviaQuestions(url);
+});
+
+function generateTriviaQuestions(url) {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      let questions = json.results;
+      for (let question of questions) {
+        let questionCard = createQuestion(question);
+        document.querySelector("main.centered").append(questionCard);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function createQuestion(question) {
+  let article = document.createElement("article");
+  article.classList.add("card");
+
+  let h2 = document.createElement("h2");
+  article.append(h2);
+  h2.textContent = question.category;
+
+  let p = document.createElement("p");
+  article.append(p);
+  p.textContent = question.question;
+
+  let correctAnswer = document.createElement("p");
+  article.append(correctAnswer);
+  correctAnswer.classList.add("hidden");
+  correctAnswer.textContent = question.correct_answer;
+
+  let button = document.createElement("button");
+  correctAnswer.before(button);
+  button.addEventListener("click", () => {
+    correctAnswer.classList.remove("hidden");
+  });
+  button.textContent = "Show Answer";
+
+  return article;
+}
