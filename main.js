fd4743aa8d139776fa4ed0693580beafceb0d7b9
lily08 <liliangelxd@gmail.com>
2022-04-05 16:03:47 -0400

    all test pass api call fetch lab

---
+++ main.js
@@ -0,0 +1,38 @@
+let form = document.querySelector('form')
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  fetch('https://opentdb.com/api.php?amount=10')
+    .then((response) => response.json())
+    .then((display) => {
+      display.results.forEach((result) => {
+        let main = document.querySelector('main.centered')
+        let article = document.createElement('article')
+        article.classList.add('card')
+
+        let h2 = document.createElement('h2')
+        h2.textContent = result.category
+
+        let p = document.createElement('p')
+        p.textContent = result.question
+
+        let p2 = document.createElement('p')
+        p2.classList.add('hidden')
+        p2.textContent = 'Correct Answer'
+
+        let button = document.createElement('button')
+        button.textContent = 'Show Answer'
+        
+        main.append(article)
+        article.append(h2, p, button, p2)
+
+        button.addEventListener('click', () => {
+          p2.classList.toggle('hidden')
+          p2.textContent = result.correct_answer
+        })
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
