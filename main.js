856ac523eeb2e413655bb849be274b98fb387f8f
named.josie <named.josie@gmail.com>
2022-04-05 22:54:14 -0400

    finished lab calls file

---
+++ main.js
@@ -0,0 +1,35 @@
+const BASE_URL = 'https://opentdb.com/api.php?amount=10';
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  fetch(BASE_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      json.results.forEach((result) => {
+        let main = document.querySelector('main.centered');
+        let article = document.createElement('article');
+        article.classList.add('card');
+        main.append(article);
+        let h2 = document.createElement('h2');
+        h2.textContent = result.category;
+        let p = document.createElement('p');
+        p.textContent = result.question;
+        let p2 = document.createElement('p');
+        p2.classList.add('hidden');
+        p2.textContent = result.correct_answer;
+        let button = document.createElement('button');
+        button.textContent = 'Show Answer';
+        article.append(h2, p, button, p2);
+      });
+
+      button.addEventListener('click', () => {
+        p2.classList.toggle('hidden');
+      });
+    })
+    .catch((ERROR) => {
+      console.log(ERROR);
+    });
+});
