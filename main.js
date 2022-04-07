06f38a094708bf7ef75d056f19f1271a58837b34
lily08 <liliangelxd@gmail.com>
2022-04-07 10:13:14 -0400

    change yml

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
 let form = document.querySelector('form')
+
+const display = ({ results }) => {
+  results.forEach((result) => {
+    let { category, question, correct_answer } = result
+    let main = document.querySelector('main')
+    let article = document.createElement('article')
+    article.classList.add('card')
+
+    let h2 = document.createElement('h2')
+    h2.textContent = category
+
+    let p1 = document.createElement('p')
+    p1.textContent = question
+
+    let button = document.createElement('button')
+    button.textContent = 'Show Answer'
+
+    let p2 = document.createElement('p')
+    p2.classList.add('hidden')
+
+    main.append(article)
+    article.append(h2, p1, button, p2)
+
+    button.addEventListener('click', (event) => {
+      event.preventDefault()
+      p2.classList.toggle('hidden')
+      p2.textContent = correct_answer
+    })
+  })
+}
+
 form.addEventListener('submit', (event) => {
   event.preventDefault()
-
   fetch('https://opentdb.com/api.php?amount=10')
     .then((response) => response.json())
-    .then((display) => {
-      display.results.forEach((result) => {
-        let main = document.querySelector('main.centered')
-        let article = document.createElement('article')
-        article.classList.add('card')
-
-        let h2 = document.createElement('h2')
-        h2.textContent = result.category
-
-        let p = document.createElement('p')
-        p.textContent = result.question
-
-        let p2 = document.createElement('p')
-        p2.classList.add('hidden')
-        p2.textContent = 'Correct Answer'
-
-        let button = document.createElement('button')
-        button.textContent = 'Show Answer'
-        
-        main.append(article)
-        article.append(h2, p, button, p2)
-
-        button.addEventListener('click', () => {
-          p2.classList.toggle('hidden')
-          p2.textContent = result.correct_answer
-        })
-      })
-    })
+    .then(display)
     .catch((error) => {
       console.log(error)
     })
 })
+
+
