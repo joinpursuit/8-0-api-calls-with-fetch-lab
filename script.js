f01d766ea2990ec3e7e06b205af491227b154ca0
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-06-04 01:27:41 -0400

    Done

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
 
+	fetch('https://opentdb.com/api.php?amount=10')
+		.then((res) => res.json())
+		.then((question) => {
+			question.results.forEach((result) => {
+				let main = document.querySelector('main.centered');
+				let article = document.createElement('article');
+				article.classList.add('card');
 
-let main = document.querySelector("main")
-let article = document.createElement("article")
-main.append(article)
+				let h2 = document.createElement('h2');
+				h2.textContent = result.category;
 
-let category = document.createElement("h2")
-article.append(h2)
-category.textContent = object.category
+				let p = document.createElement('p');
+				p.textContent = result.question;
 
-let question = document.createElement("p")
-article.append(p)
-question.textContent = object.question
+				let p2 = document.createElement('p');
+				p2.classList.add('hidden');
+				p2.textContent = 'Correct Answer';
 
-let button = document.createElem("button")
-article.append(button)
+				let button = document.createElement('button');
+				button.textContent = 'Show Answer';
 
+				main.append(article);
+				article.append(h2, p, button, p2);
 
-let correctAnswer = document.createElement("p")
-article.append(p2)
-
-
-
-
-console.log(data.results)
-
-fetch(https://opentdb.com/api_config.php)
+				button.addEventListener('click', (event) => {
+					p2.classList.toggle('hidden');
+					p2.textContent = result.correct_answer;
+				});
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
