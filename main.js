4054d64611a1b446e9a01d63f53a6de49838247f
PESolut <jahaadpetty@pursuit.org>
2022-08-29 14:02:25 -0400

    linked html, saved our base-url to variable, selected our form and listened to form submission

---
+++ main.js
@@ -0,0 +1,25 @@
+// console.log('test')
+
+// put our API base URL here as a const
+const BASE_URL='https://opentdb.com/api.php'
+
+// selecting our form
+const form=document.querySelector('form')
+// console.log(form)
+
+// listening to form submission 
+
+
+
+// log
+/// 128pm linking this file to our html file
+//  129pm testing via console.log, SUCCESS
+// 130pm putting our base url
+// 147pm selecting our form
+// 149pm SUCCESS
+// 154pm listening to form submission
+
+//plan 
+
+// after listening to form submission, on submit, create the framework of our card using DOM MANIPUALTION
+// then after creating the framework, append to our main with the class of centerted
