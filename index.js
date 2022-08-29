59ce816780235111b0e9d192a12e589969acb62a
TonyH98 <tonyhoang@pursuit.org>
2022-08-29 17:36:52 -0400

    All tests passed

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 
-for(let i = 0; i <10; i++){
-const main = document.querySelector("main")
-const article = document.createElement("article")
-article.setAttribute("class", "card")
-const h2 = document.createElement("h2")
-const paragraph = document.createElement("p")
-const paragraph2 = document.createElement("p")
-const button = document.createElement("button")
-button.setAttribute("type", "submit")
-button.textContent = `Show Answer`
-article.append(h2, paragraph, button, paragraph2)
-main.append(article)
+// for(i = 0; i <10; i++)
+// const main = document.querySelector("main")
+// const article = document.createElement("article")
+// article.setAttribute("class", "card")
+// const h2 = document.createElement("h2")
+// const paragraph = document.createElement("p")
+// const paragraph2 = document.createElement("p")
+// const button = document.createElement("button")
+// button.textContent = `Show Answer`
+// article.append(h2, paragraph, button, paragraph2)
+// main.append(article)
 
 
 
 
 const form = document.querySelector("form")
 
-let category = 0
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    fetch(`https://opentdb.com/api.php?amount=10&category=${category}`)
+    fetch(`https://opentdb.com/api.php?amount=10`)
     .then((response) => response.json())
     .then((resJson) => {
         const results = resJson.results
         results.forEach((questions) => {
+          const main = document.querySelector("main")
+const article = document.createElement("article")
+article.setAttribute("class", "card")
+const h2 = document.createElement("h2")
+const paragraph = document.createElement("p")
+const paragraph2 = document.createElement("p")
+const button = document.createElement("button")
+button.textContent = `Show Answer`
+article.append(h2, paragraph, button, paragraph2)
+main.append(article)
+
+
             paragraph.textContent = questions.question
             h2.textContent = questions.category
-
-            article.addEventListener("click", () => {
-                paragraph2.classList.toggle(`hidden`)
+ article.addEventListener("click", () => {
                        if(paragraph2.classList.contains("hidden")){
                          paragraph2.innerHTML = ""
                        }
@@ -44,5 +53,5 @@ form.addEventListener("submit", (event) => {
     .catch((error) => console.log(error))
 })
 
-}
+
 
