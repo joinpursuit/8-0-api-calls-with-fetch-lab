6febee703d57148652b8c180d8c4ed0322db5fe1
Bo Latt <bolattny@gmail.com>
2022-08-29 21:57:53 -0400

    bonus 2 : add  select difficulty

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-const URL = "https://opentdb.com/api.php?amount=10";
+const baseURL = "https://opentdb.com/api.php?amount=10";
 const form = document.querySelector("form");
 const main = document.querySelector("main");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   deleteOldQuestions();
-  getData(URL);
+  const URL = createURL(baseURL, e.target.difficulty.value);
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => createAndAppendCard(data.results))
+    .catch(console.log);
 });
 
-const getData = (url) => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => createCard(data.results))
-    .catch(console.log);
-};
-
-const createCard = (questions) => {
+const createAndAppendCard = (questions) => {
   for (let question of questions) {
-    console.log(question);
+    // console.log(question);
     const article = document.createElement("article");
     article.classList.add("card");
     article.classList.add(question.difficulty);
@@ -36,7 +33,7 @@ const createCard = (questions) => {
 
     const p2 = document.createElement("p");
     p2.classList.add("hidden");
-    p2.textContent = question.correct_answer;
+    p2.textContent = decodeHtmlEntity(question.correct_answer);
     article.append(h2, p, button, p2);
 
     main.append(article);
@@ -51,4 +48,8 @@ const decodeHtmlEntity = (html) => {
   var txt = document.createElement("textarea");
   txt.innerHTML = html;
   return txt.value;
+};
+
+const createURL = (base, diffLevel) => {
+  return base + "&difficulty=" + diffLevel;
 };
