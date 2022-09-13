0f27b2f0d7a5aee5773c524d8793e4c17462a02b
shaquala <shaqualafredericks@pursuit.org>
2022-09-13 00:03:04 -0400

    tests passed

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
-let amount = 10
-const getNewQuestions = () => {
-    TriviaQuestion.innerHTML = ""
-   // console.log("I have been clicked")
-    //base url: https://opentdb.com/api.php?amount=10
+const form = document.querySelector("form");
+const URL_PAGE = `https://opentdb.com/api.php?amount=10` ;
+const questionsList = document.querySelector("main.centered");
+
+const updatePage =(question) => {
+
+  let article = document.createElement("article");
+  article.classList.add("card");
+
+  let category = document.createElement("h2");
+  category.textContent = `${question.category}`;
+  article.append(category);
+  
+  let questionText = document.createElement("p");
+  questionText.textContent =`${question.question}`;
+  article.append(questionText);
+
+  let showAnswerButton = document.createElement("button");
+  article.append(showAnswerButton);
+
+  let answer = document.createElement("p");
+  answer.classList.add("hidden");
+  answer.textContent = `${question.correct_answer}`;
+  article.append(answer);
+
+  showAnswerButton.addEventListener("click", (event) => {
+    event.target.classList.toggle("hidden");
+  });
+questionsList.append(article);
+};
+
+const showError = (error) =>{
+  console.log(error);
+  let err = document.createElement("p");
+  err.textContent = `${error}`;
+  body.prepend(err);
+};
+
+form.addEventListener("submit", (event)=> {
+  event.preventDefault();
+
+  fetch(URL_PAGE)
+  .then((result)=> result.json())
+  .then((json)=> {
+    json.results.forEach((obj) => updatePage(obj));
+  })
+  .catch(showError);
+});
+
+
+
+
 
     
 
-  const button = document.querySelector('button')
-  console.log(button) 
-  const getNewQuestions = document.querySelector('getNewQuestions')
-
-  button.addEventListener("click", () => {
-    amount+= 10
-    getNewQuestions()
-    console.log(amount)
-  })
-    
-}
-    fetch(`https://opentdb.com/api.php?amount=10`)
-    .then((res)=> res.json ())
-    .then((resJson)=> {
-        console.log(resJson.results)
-        const results = resJson.results
-        results.forEach((result)=> {
-            console.log(result)
-
-  })
-            
-
-    })
-            
-let article = document.createElement('article')
-article.classList.add('card')
-     
- let h2 = document.createElement('h2')
-     h2.textContent = question.category
-     article.append(h2)
-
-let questionText = document.createElement('p')
-     questionText.textContent=question.question
-     article.append(questionText)
-
-let showAnswerButton = document.createElement('button')
-     showAnswerButton.textContent = 'Show Answer'
-     article.append(showAnswerButton)
-let hiddenAnswer = document.createElement ('p')
-     hiddenAnswer.classList.add('hidden')
-     hiddenAnswer.textContent = question.correct_answer
-     article.append(hiddenAnswer)
-     main.append(article)
-     
-     let form = document.querySelector('form')
-form.addEventListener('submit', event => {
-    event.preventDefault()
-    fetchgetNewQuestions(baseURL)
-
-})
-     
- fetchgetNewQuestions(baseURL)  
+  
