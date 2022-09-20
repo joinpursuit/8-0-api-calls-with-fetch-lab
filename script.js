9bcf840b8177f1e11492c58ebca88c8d72bf6c6b
Tech-Kat <katpelle@pursuit.org>
2022-09-20 17:57:55 -0400

    fetchDat

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
-const BASE_URL = `https://opentdb.com/api_config.php?amount=10&category=11&difficulty=easy&type=multiple"`
-
-{/* <article class="card">
-  <h2>CATEGORY</h2>
-  <p>QUESTION</p>
-  <button>Show Answer</button>
-  <p class="hidden">CORRECT ANSWER</p>
-</article> */}
-
-function moreQuestions() {
-    const article = document.createElement("article");
-    article.classList.add(".card");
-    
-    const h2 = document.createElement("h2");
-    article.append(h2);
-
-    const p = document.createElement("p");
-    article.append(p);
-    p.textContent = "";
-
-    const button = document.createElement("button");
-    article.append(button);
-
-    const p2 = document.createElement("p2");
-    p2.classList.add("hidden");
-    p2.textContent = "";
-    article.append(p2);
-
-    
-}
+const BASE_URL = `https://opentdb.com/api.php?amount=10`
 
 
-function fetchThat() {
-fetch(BASE_URL)
-    .then(response => response.json())
-    .then(console.log)
-    .catch(console.log)
-}
 
+const main = document.querySelector("main");
 const form = document.querySelector("form");
-form.addEventListener("click", (event) => {
-    event.preventDefault();
 
 
-})
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    moreQuestions(BASE_URL)
+    main.innerHTML = ""
 
-const article = document.querySelector("article");
+    });
+    function moreQuestions(BASE_URL) {
+        fetch(BASE_URL)
+        .then(response => response.json())
+        .then((fetchdata) => {
+            console.log(fetchdata)
+            for(let f of fetchdata.results){
+                const article = document.createElement("article");
+                article.classList.add("card");
+                main.append(article);
+                
+                const h2 = document.createElement("h2");
+                article.append(h2);
+                h2.textContent = f.category
+            
+                const p = document.createElement("p");
+                article.append(p);
+                p.innerHTML = f.question;
+            
+                const button = document.createElement("button");
+                article.append(button);
+                button.textContent = "Show Answer";
+            
+                const p2 = document.createElement("p2");
+                p2.classList.add("hidden");
+                article.append(p2);
+                p2.innerHTML = f.correct_answer;
+               
+            
+                button.addEventListener('click', () => {
+                    p2.classList.remove("hidden")
+                });
+            }
+            })
+       
+        
+            .catch((err) => {
+                console.log(err);
+            });
+    
+        }
+        
+        
 
 
-const questions = document.createElement("p")
+            
+
+
+        
+        
+
+
+
+
+
+
+
+
+
