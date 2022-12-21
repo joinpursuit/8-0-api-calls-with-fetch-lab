1219eda6635f344fc7491a2fd0ae4638ebf5a335
SuperNinjaEv <evan0210@gmail.com>
2022-12-20 23:06:22 -0500

    Added difficulty coloration, working on answer button functionality

--- main.js
+++ main.js
@@ -5,26 +5,44 @@ const BASE_URL = `https://opentdb.com/api.php?amount=10`
 
 button.addEventListener('click', async (event) => {
     event.preventDefault();
-    console.log('Button was clicked');
+
+    if (document.querySelector('article')) {
+        let articles = document.querySelectorAll('article');
+        for (let article of articles) {
+            article.remove();
+        }
+    }
+    console.log('Button was clicked!');
     await fetch(BASE_URL)
+
         .then((response) => response.json())
+        
         .then((data) => {
             console.log(data);
-            let mainTag = document.querySelector('main');
-            for (let result of data.results) {
-                let categories = result.category;
-                let difficulty = result.difficulty;
-                let quest = result.question;
-                let correctAnswer = result.correct_answer;
-                let divTag = document.createElement('div');
-                mainTag.appendChild(divTag);
-                divTag.innerHTML = `
-            <article class ="card">
-            <h2>${categories}</h2>
+            
+            const questions = data.results;
+            
+            for (let question of questions) {
+                // Helpful buckets with values from data JSON
+                let category = question.category;
+                let difficulty = question.difficulty;
+                let quest = question.question;
+                let correctAns = question.correct_answer;
+                let newArticle = document.createElement('article');
+                newArticle.classList.add('card')
+                mainTag.appendChild(newArticle);
+// Changes each articles/question border color based upon difficulty
+                if (difficulty === 'medium') {
+                    newArticle.style.borderColor = 'yellow';
+                } else if (difficulty === 'hard') {
+                    newArticle.style.borderColor = 'red';
+                }
+
+                newArticle.innerHTML = `
+            <h2>${category}</h2>
             <p>${quest}</p>
             <button>Show Answer</button>
-            <p class="hidden">${correctAnswer}</p>
-            </article>`
+            <p class="hidden">${correctAns}</p>`
             }
         })
         .catch((error) => console.log(error))
