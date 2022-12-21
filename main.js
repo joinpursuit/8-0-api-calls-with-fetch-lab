24a3e3171286504f63a872bf91f2c4bb0ab2ee64
BertCasale <bthebest55@gmail.com>
2022-12-20 20:25:29 -0500

    finished first part

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const BASE_URL = "https://opentdb.com/api.php?amount=10";
 
 //get the button
 const questionButton = document.querySelector("button");
+const main = document.querySelector("main")
 
 questionButton.addEventListener("click", async (event) => {
     //prevent the button from doing what it normally would
@@ -18,7 +19,28 @@ questionButton.addEventListener("click", async (event) => {
             let questions = data.results;
             //loop through array
             for (let i = 0; i < questions.length; i++){
+                //create the article tag for the questions to go in
+                let article = document.createElement("article");
+                //give it a class of card
+                article.classList.add("card");
+
+                //make a variable for the actual question
+                let question = `
+                <h2>${questions[i].category}</h2>
+                <p>${questions[i].question}</p>
+                <button>Show Answer</button>
+                <p class="hidden">${questions[i].correct_answer}</p>`
+                //give the article the question data as the innerHTML
+                article.innerHTML = question;
+                //put the question inside at the end of main
+                main.append(article);
+
+
+                //ANSWER BUTTON
                 
+
+
+
             }
 
 
