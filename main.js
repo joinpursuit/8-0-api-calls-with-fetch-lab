c1b15522f8359267f3729ef657f934ff65174f90
BertCasale <bthebest55@gmail.com>
2022-12-20 21:39:44 -0500

    lab complete

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const BASE_URL = "https://opentdb.com/api.php?amount=10";
 
 //get the button
 const questionButton = document.querySelector("button");
+//get the main section
 const main = document.querySelector("main")
 
 questionButton.addEventListener("click", async (event) => {
     //prevent the button from doing what it normally would
     event.preventDefault();
 
-    //fecth the url
+    //fetch the url
     await fetch(BASE_URL)
         .then((res) => res.json())
         //get the data
@@ -21,15 +22,15 @@ questionButton.addEventListener("click", async (event) => {
             for (let i = 0; i < questions.length; i++){
                 //create the article tag for the questions to go in
                 let article = document.createElement("article");
-                //give it a class of card
-                article.classList.add("card");
+                //give it a class of card and answer at i
+                article.classList.add("card", `answer${i}`);
 
                 //make a variable for the actual question
                 let question = `
                 <h2>${questions[i].category}</h2>
                 <p>${questions[i].question}</p>
                 <button>Show Answer</button>
-                <p class="hidden">${questions[i].correct_answer}</p>`
+                <p class="hidden answer${[i]}">${questions[i].correct_answer}</p>`;
                 //give the article the question data as the innerHTML
                 article.innerHTML = question;
                 //put the question inside at the end of main
@@ -37,18 +38,20 @@ questionButton.addEventListener("click", async (event) => {
 
 
                 //ANSWER BUTTON
-                
+                //get the answer button
+                let answerButton = document.querySelector(`.answer${[i]} button`);
 
+                //get the answer paragraph
+                let answer = document.querySelector(` p.answer${[i]}`);
 
-
+                //tell button what to do when clicked
+                answerButton.addEventListener("click", () => {
+                    //change the class on that paragraph
+                    answer.classList.toggle("hidden");
+                })
             }
-
-
-
-
         })
         .catch((err) => {
             console.log(err);
         })
-
 })
