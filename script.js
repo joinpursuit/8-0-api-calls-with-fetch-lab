2892b44d3b3ef3dae85e1bfbfb59e00441a18705
JenniferEinstein <jennifereinstein@gmail.com>
2022-12-20 22:16:26 -0500

    as far as I can take it

--- script.js
+++ script.js
@@ -3,45 +3,49 @@
 
 const BASE_URL = 'https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=multiple';
 const API_KEY = "d6c670c28f93644a33f7cfd69726a0cb2ce5d7f8be70dedea0ef784030dc6d25"
-const questionButton = document.querySelector("button");
-const main = document.querySelector("main"); //this is where we were instructed to have the card show up
-const answerButton = document.querySelector("button.card");
 
+//get the button
+const questionButton = document.querySelector("button");
+//get the main section
+const main = document.querySelector("main"); //this is where we were instructed to have the card show up
 
 questionButton.addEventListener('click', async (event) => {
-  event.preventDefault()
+  event.preventDefault(); //prevent the button from normal action
 
     await fetch(BASE_URL)
       .then((res) => res.json())
+      //get the data and then do something with it
       .then((response) => {
-        console.log(response)
-
-        let questions  = response.results
+        console.log(response);
+//save the array to a variable
+        let questions  = response.results;
+//loop through array
         for (let i=0; i<questions.length; i++){
+//create article tag for questions to go in          
             let article = document.createElement("article");
-            article.classList.add("card");
-
-
+            article.classList.add("card", `answer${i}`);
+//make a variable for the actual question
           let question = `       
           <h2>${questions[i].category}</h2>
           <p>${questions[i].question}</p>
           <button>Show Answer</button>
-          <p class="hidden">${questions[i].correct_answer}</p>`
-
+          <p class="hidden">${questions[i].correct_answer}</p>`;
+//give article question data via innterHRML
           article.innerHTML = question;
+//put each question at the end of main          
           main.append(article);
-        }
-      })
-      .catch((error) => {
-      console.log(error)
-    })
-  })
-//what should this button do when clicked?
-answerButton.addEventListener('click', async (event) => 
-{
-  //get the answer paragraph
-  let answer = document.querySelector(".hidden");
-  answer.classList.toggle("hidden");
 
-})
+        let answerButton = document.querySelector(`.answer${[i]} button`);
+        let answer = document.querySelector(`p.answer${[i]}`);
+
+        answerButton.addEventListener ('click', () => {
+//change the class on that paragraph
+        answer.classList.toggle("hidden");
+      })
+    }
+  })
+  .catch((error) => {
+  console.log(error)
+  });
+});
   
