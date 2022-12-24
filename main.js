880b5cae597e65df2c723ebf750a2dbf6092c2cf
christinacephus@pursuit.org <christinacephus@pursuit.org>
2022-12-23 23:24:59 -0500

    restructuring-- added ascii text edit, corrected vars

--- main.js
+++ main.js
@@ -8,45 +8,44 @@ addNew.addEventListener ("submit", event => {
 
 function display (BASE_URL) {
 
-    const BASE_URL_RESPONSE = fetch( BASE_URL);
-    const myResponse = BASE_URL_RESPONSE.then( response => response.json() );
-
-    myResponse.then(({results}) => {
-
-    for(let card of theCard) {
-const {category, question, correct_answer} = theCard; //create
-const myMain = document.querySelector("main") // update
-
-const article = document.createElement("article");//create
+    fetch( BASE_URL)
+    .then( response => response.json() )
+    .then(({results}) => { //extracting the results of the api --> results will go into array
+        results.forEach(card => {
+            newCard(card) // this is the function we are creating
+        })
+    })
+}
+//create our function newCard
+function newCard(card) {
+    //object destructuring
+const {category, question, correct_answer} = card;
+const myMain = document.querySelector("main") // update -- extracting main element from dom and adding it to a variable.
+const article = document.createElement("article");//create 
 article.classList.add("card"); //update
-
 const hdg2 = document.createElement("h2"); //add functionality
-heading2.innerText = category;
-
+hdg2.innerText = category;
 article.append(hdg2); //append
 
 const pregunta = document.createElement("p");
-pregunta.innerText = question.replace(/&quot;/g, '"');
-
+pregunta.innerText = question.replace(/&quot;/g, '"');// &quot is double quotes -- takes in string values based on ASCII value
+pregunta.innerText = question.replace(/&#039;/g, "'"); 
 article.append(pregunta); //append
 
-const solution = docuemnt.createElement("button");
+const solution = document.createElement("button");
 solution.innerText = "Show Answer";
-
 article.append(solution); //append
 
 const answerKey = document.createElement("p"); //create
-solution.classList.add("hidden"); //update
-solution.innerText = correct_answer; // functionality
+answerKey.classList.add("hidden"); //update
+answerKey.innerText = correct_answer; // functionality
 
-
-article.append(solution); //append
+article.append(answerKey); //append
 myMain.append(article);
-
 solution.addEventListener("click", kiwi => {
     kiwi.target.parentNode.lastChild.classList.remove("hidden");
+    
 });
+    
 }
-}).catch(displayError);
-};
 function displayError(error){};
