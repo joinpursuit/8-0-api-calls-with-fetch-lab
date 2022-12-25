878eab49dfa948d26244a479069c12938885fa38
Diandre Miller <millerdiandre@gmail.com>
2022-12-24 23:34:53 -0500

    Completed API Calls with Fetch Lab

---
+++ main.js
@@ -0,0 +1,91 @@
+const BASE_URL = "https://opentdb.com/api.php?amount=10";
+
+const form = document.querySelector('form');
+form.addEventListener('submit', submitEvent => {
+    submitEvent.preventDefault();
+    triviaQuestions(BASE_URL)
+})
+
+function triviaQuestions(url){
+    fetch(url)
+        .then((response) => response.json())
+        .then(({results}) => { // results is destructuring object
+            console.log(results) // an array of cards
+            results.forEach(element => { createCard(element)    
+            });
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+function createCard(card){
+    const { question, correct_answer, category, difficulty, incorrect_answers } = card // destructuring object
+    
+    // const getQuestions = document.querySelector('.a');
+    // console.log(getQuestions)
+
+    // getQuestions.addEventListener('click', handleClick => {
+    //     handleClick.setAttribute('display', 'block');
+    // });
+
+
+
+    const main = document.querySelector("main")
+    const article = document.createElement("article");
+    article.setAttribute('class', 'card');
+
+    const h2 = document.createElement("h2");
+    
+    const pQuestion = document.createElement("p");
+    pQuestion.innerText = question.replaceAll('&quot','"').replaceAll('&#039',"'").replaceAll('&amp','&').replaceAll(';','')
+
+    const pAnswer = document.createElement("p");
+    pAnswer.classList.add("hidden");
+    pAnswer.innerText = correct_answer;
+
+    const button = document.createElement("button");
+    button.addEventListener("click",handleClick => {
+        pAnswer.classList.remove("hidden")
+    })
+    
+    article.append(h2, pQuestion, button, pAnswer);
+    main.append(article);
+
+    // if (difficulty === 'easy') {
+    //     pQuestion = ''
+    //     pAnswer = ''
+    //     pQuestion.forEach(question => {
+    //     question.type.value === 'medium'
+    //     })
+    // }
+    
+    // if (difficulty === 'medium') {
+    //     pQuestion = ''
+    //     pAnswer = ''
+    //     pQuestion.forEach(question => {
+    //     question.type.value === 'medium'
+    //     })
+    // }
+    
+    
+    
+    // if (difficulty === 'hard') {
+    //     pQuestion = ''
+    //     pAnswer = ''
+    //     pQuestion.forEach(question => {
+    //         question.type.value === 'hard'
+    //     })
+       
+    // }
+
+}
+
+
+
+    
+
+    
+
+
+        
