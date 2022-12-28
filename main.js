e25a23bc646e962bf31783f7a56f72f64c184d07
shaquala <96317236+shaqfred@users.noreply.github.com>
2022-12-27 21:44:57 -0500

    completed the base requirements-passed both tests

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form");
 const URL_PAGE = `https://opentdb.com/api.php?amount=10`;
 const questionsList =document.querySelector("main.centered");
 
-console.log(URL_PAGE)
+
 
 const updatePage =(question) => {
     
@@ -12,4 +12,40 @@ const updatePage =(question) => {
     let category = document.createElement("h2");
     category.textContent = `${question.category}`;
     article.append(category);
-}
+
+    let questionText = document.createElement("p");
+    questionText.textContent = `${question.question}`;
+    article.append(questionText);
+
+    let showAnswerButton = document.createElement("button");
+    article.append(showAnswerButton);
+
+    let answer = document.createElement("p");
+    answer.classList.add("hidden");
+    answer.textContent = `${question.correct_answer}`;
+    article.append(answer);
+
+    showAnswerButton.addEventListener("click", (event)=> {
+        event.target.classList.toggle("hidden");
+    });
+    questionsList.append(article);
+
+};
+
+const showError = (error) => {
+    console.log(error);
+    let err =document.createElement("p");
+    err.textContent = `${error}`;
+    body.prepend(err);
+};
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+
+    fetch(URL_PAGE)
+    .then((result) => result.json())
+    .then((json) => {
+        json.results.forEach((obj) => updatePage(obj));
+    })
+    .catch(showError);
+});
