e43f31fdd70440420cd6a64411d51941a2e4d351
Diegocarrero <diegocar2413@gmail.com>
2023-01-17 19:12:50 -0500

    Now reveals the correct answers

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const API_URL = 'https://opentdb.com/api.php?amount=10'
 const button = document.querySelector('button')
 const main = document.querySelector('main')
+const color = document.getElementById('card')
 
 button.addEventListener('click', (event) => {
     event.preventDefault()
-    fetch(`https://opentdb.com/api.php?amount=10`)
+
+    fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
-        console.log(data)
-        let info = data.results
+        console.log(data);
+        let info = data.results;
         for(let i = 0; i < info.length; i++){
-        const newArticle = document.createElement('article')
-        newArticle.setAttribute('class','card')
-        newArticle.innerHTML = `
-        <h2>${info[i].category}</h2>
-        <p>${info[i].question}</p>
-        <button>Show Answer</button>
-        <p class="hidden">${info[i].correct_answer}</p>
-        `
-        main.append(newArticle)
-     }
-    });
-    
+            const newArticle = document.createElement('article')
+            newArticle.setAttribute('class','card')
+            newArticle.setAttribute('class', `answer${i}`)
+            newArticle.innerHTML = `
+            <h2>${info[i].category}</h2>
+            <p>${info[i].question}</p>
+            <button>Show Answer</button>
+            <p class="hidden answer${i}">${info[i].correct_answer}</p>
+            `;
+            main.append(newArticle);
+
+            const answerButton = document.querySelector(`.answer${i} button`);
+            const answerP = document.querySelector(`p.answer${i}`);
+
+            answerButton.addEventListener('click', () => {
+                answerP.classList.toggle('hidden');
+            })
+        }
+    })
+    .catch((error) => console.log(error));
 })
